8fc0bafcac594257ed9d7d3501d2fa965d52fb96
FahriSarung <fahricloudhost@gmail.com>
2023-07-02 20:17:29 +0700

    project

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//Navbar hamburger
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+  navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("active");
+}
